0f571c8010b9e3dcad6626d61157e9629a64d2a3
Jean-Michel <jeanmichel.bakker@gmail.com>
2019-09-28 20:57:25 +0200

    show todo lists

--- client/src/app/todolists/todolists.component.ts
+++ client/src/app/todolists/todolists.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Router } from '@angular/router';
+import { CoreService } from "../socket.service";
 
 @Component({
-  selector: 'app-todolists',
-  templateUrl: './todolists.component.html',
-  styleUrls: ['./todolists.component.css']
+  selector: "app-todolists",
+  templateUrl: "./todolists.component.html",
+  styleUrls: ["./todolists.component.css"]
 })
 export class TodolistsComponent implements OnInit {
+  private items = [{name: 'list1'}];
 
-  constructor() { }
+  constructor(private coreService: CoreService, private router: Router) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
 }
