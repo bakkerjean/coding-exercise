02b9b4486ed7965691e6bfbca08685eaf8f4be65
Jean-Michel <jeanmichel.bakker@gmail.com>
2019-09-28 19:26:58 +0200

    setup websockets and auth function

--- server/app.js
+++ server/app.js
@@ -5,13 +5,41 @@ const WSAuth = require("socketio-auth");
 const httpServer = http.createServer();
 const io = new WSServer(httpServer);
 
-httpServer.listen(3000, function onListenStarted() {
-  console.log("Server started");
+httpServer.listen(3000);
+
+const auth = new WSAuth(io, {
+  authenticate: authenticate,
+  timeout: 1000000
 });
 
-io.on("connection", function(socket) {
-  socket.emit("news", { hello: "world" });
-  socket.on("my other event", function(data) {
-    console.log(data);
+const sockets = {};
+
+io.on("connection", socket => {
+  console.log("connected");
+
+  socket.on("disconnecting", reason => {
+    console.log(reason);
+    removeSocket(socket);
+  });
+
+  socket.on("disconnect", reason => {
+    console.log(reason);
+  });
+
+  socket.on("error", err => {
+    console.log(err);
   });
 });
+
+function removeSocket(socket) {
+  if (sockets[socket.id]) {
+    delete sockets[socket.id];
+  }
+}
+
+function authenticate(socket, data, cb) {
+  console.log("authenticating");
+  const login = JSON.parse(data);
+  const authPassed = login.userName === "user" && login.password === "pass";
+  cb(null, authPassed);
+}
