69b212acc38aee18732966bcdb3d8bfca52b5f5d
Jean-Michel <jeanmichel.bakker@gmail.com>
2019-09-28 20:11:37 +0200

    routing

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { LoginComponent } from "./login/login.component";
+import { TodolistsComponent } from "./todolists/todolists.component";
 
 const routes: Routes = [
   { path: "", redirectTo: "/login", pathMatch: "full" },
-  { path: "login", component: LoginComponent }
+  { path: "login", component: LoginComponent },
+  { path: "todolists", component: TodolistsComponent }
 ];
 
 @NgModule({
