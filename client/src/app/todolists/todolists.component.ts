749c6c783abf187917cea37d19b065a6e913daa4
Jean-Michel <jeanmichel.bakker@gmail.com>
2019-09-28 20:11:05 +0200

    added todolists component

---
+++ client/src/app/todolists/todolists.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todolists',
+  templateUrl: './todolists.component.html',
+  styleUrls: ['./todolists.component.css']
+})
+export class TodolistsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
